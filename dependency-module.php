803b4433d2b656bdb7e8ddc2a155e0ba6da05dd3
Employee 451 <mail@askehippebrun.com>
2017-12-28 13:43:26 +0100

    Version 1

--- dependency-module.php
+++ dependency-module.php
@@ -5,7 +5,7 @@ Plugin URI: http://github.com/employee451/dependency-module
 Description: This plugin delivers the essential dependencies for Employee 451 Pixelarity themes.
 Author: Employee 451
 Author URI: http://employee451.com/
-Version: 0.0.1
+Version: 1.0
 GitHub Plugin URI: employee451/dependency-module
 */
 
